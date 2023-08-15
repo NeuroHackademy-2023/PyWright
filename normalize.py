cb7e50353fede08fb0f92a053fd0a58362a1780e
Shir Dahan <shir9294@gmail.com>
2023-08-15 20:17:54 +0100

    changes

--- normalize.py
+++ normalize.py
@@ -1,15 +1,15 @@
-# Inputs:
-- `matrix`: structural connectivity matrix
-- `do_norm`: 0 = no normalization, 1 = binarize, 2 = normalize
-- *optional* `bin_thresh`: default = o, otherwise = min> threshold >max
+#Inputs:
+#- `matrix`: structural connectivity matrix
+#- `do_norm`: 0 = no normalization, 1 = binarize, 2 = normalize
+#- *optional* `bin_thresh`: default = o, otherwise = min> threshold >max
 
 # Output:
-- norm: the normalized matrix
+#- norm: the normalized matrix
 
 # Code:
-load the matrix
+#load the matrix
 
-```
+
 def normalize_matrix (matrix, do_norm = 0, bin_thresh = 0):
 mini = matrix.min()
 maxi = matrix.max()
@@ -22,13 +22,12 @@ maxi = matrix.max()
         elif mini <= bin_thresh <= maxi:
             threshold = bin_thresh
 	else:
-	    print('Threshold must be in values range. 
+	    print('Threshold must be in values range.
 	    matrix minimum value > bin_thresh > matrix maxmimum value')
     return matrix = matrix[matrix <= threshold] = 0;
-    elif do_norm = 2:
+    elif do_norm == 2:
         print ('Normalization of structural matrix')
     return matrix = matrix/maxi
     else:
-        print('Specify normalization input. 0 = no normalization, 1 = binarize, 2 = 					normalize')
-```
- 
+        print('Specify normalization input. 0 = no normalization, 1 = binarize, 2 = normalize')
+
